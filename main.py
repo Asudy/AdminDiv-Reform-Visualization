61e0ed093e0b4102cd13d8354a7ed8bb62263207
Asudy <18906052155@189.cn>
2021-05-21 17:01:42 +0800

    feat(main): add display of province map of the selected year

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from altair.vegalite.v4.schema.core import Projection
 import streamlit as st
 import pandas as pd
+import geopandas as gpd
+import altair as alt
 
 st.set_page_config(
     page_title = "中国行政区划改革"
@@ -14,21 +17,31 @@ st.title("中国行政区划改革")
 st.sidebar.header("选择年份")
 year = st.sidebar.slider("年份", min_value=1970, max_value=2020, value=2019)
 st.sidebar.header("选择行政区")
-view = st.sidebar.select_slider("视图", ["国", "省", "市"], value='市')
+view = st.sidebar.select_slider("视图", ['国', '省', '市'], value='省')
 province, city = "", ""
 if view == "省" or view == "市":
     province = st.sidebar.selectbox("省份", Provinces)
 if view == "市":
     city = st.sidebar.selectbox("城市", Cities)
+# province = st.sidebar.selectbox("省份", Provinces)
+
+## 失败的读 shp 文件代码，暂弃
+# df = gpd.read_file("data/countypoint_1996_湖北省_sample.shp")
+# # st.write(df.head())
+# inline_data = alt.InlineData(df.to_json())
+# chart = alt.Chart(inline_data).mark_geoshape()
+# st.altair_chart(chart)
 
 
 st.write("选中年份：{}；省份：{}；城市：{}".format(year, province, city))
 st.header("所选行政区地图展示")
-st.image("img/Lenna.png", "行政区划图：{}, {}{}".format(year, province, city))
+# st.image("img/Lenna.png", "行政区划图：{}, {}{}".format(year, province, city))
+# st.image("data/countypoint_1996_湖北省_sample.shp", "行政区划图：{}, {}{}".format(year, province, city))
+st.image("img/{}/province/{}.png".format(year, province), "行政区划图：{}年, {}{}".format(year, province, city))
 
 st.header("行政区划具体变化")
 st.markdown("**{}：**2019年末，全省常住人口3973万人，其中城镇常住人口2642万人。\
-    辖9个地级市，29个市辖区、12个县级市、44个县（合计85个县级行政区划单位），\
-        184个街道、653个镇、251个乡、19个民族乡（合计1107个乡级行政区划单位），\
+            辖9个地级市，29个市辖区、12个县级市、44个县（合计85个县级行政区划单位），\
+            184个街道、653个镇、251个乡、19个民族乡（合计1107个乡级行政区划单位），\
             2649个居委会、14355个村委会。".format(province))
 st.markdown("> 读xlsx还没有做所以现在选不同行政区这里还不会变orzzz")
