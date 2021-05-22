8aaac5ecdec4f1e1e7d0b982de38444b9853ba0c
Asudy <18906052155@189.cn>
2021-05-22 17:53:22 +0800

    feat(main): display maps and details
    - Add map display of a selected year & province.
    - Read Excel data and display details of changing in a table.

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
-from altair.vegalite.v4.schema.core import Projection
+from numpy import e
 import streamlit as st
 import pandas as pd
-import geopandas as gpd
-import altair as alt
+from GetProvinceNamesByYear import GetProvinceNamesByYear
+import ReadExcel
 
 st.set_page_config(
-    page_title = "中国行政区划改革"
+    page_title = "中国行政区划改革",
+    initial_sidebar_state="expanded"
 )
 
-
-Provinces = ['福建', '省份1', '省份2', '省份3', '省份4', '省份5']
+# Provinces = ['福建', '省份1', '省份2', '省份3', '省份4', '省份5']
 Cities = ['厦门', '城市1', '城市2', '城市3', '城市4', '城市5']
 
+### Set title
 st.title("中国行政区划改革")
 
+### Set sidebar
 st.sidebar.header("选择年份")
-year = st.sidebar.slider("年份", min_value=1970, max_value=2020, value=2019)
-st.sidebar.header("选择行政区")
-view = st.sidebar.select_slider("视图", ['国', '省', '市'], value='省')
-province, city = "", ""
-if view == "省" or view == "市":
-    province = st.sidebar.selectbox("省份", Provinces)
-if view == "市":
-    city = st.sidebar.selectbox("城市", Cities)
-# province = st.sidebar.selectbox("省份", Provinces)
+year = st.sidebar.slider("年份", min_value=1977, max_value=2020, value=1980)
+Provinces = GetProvinceNamesByYear(year)
+st.sidebar.header("选择省份")
+province = st.sidebar.selectbox("省份", Provinces)
+## 只能选省份了，以下代码弃用
+# view = st.sidebar.select_slider("视图", ['国', '省', '市'], value='省')
+# province, city = "", ""
+# if view == "省" or view == "市":
+#     province = st.sidebar.selectbox("省份", Provinces)
+# if view == "市":
+#     city = st.sidebar.selectbox("城市", Cities)
 
 ## 失败的读 shp 文件代码，暂弃
 # df = gpd.read_file("data/countypoint_1996_湖北省_sample.shp")
@@ -33,15 +37,25 @@ if view == "市":
 # st.altair_chart(chart)
 
 
-st.write("选中年份：{}；省份：{}；城市：{}".format(year, province, city))
+## Display info (map && detail)
+st.code("选中年份：{}；视图：；省份：{}；城市：".format(year, province))
 st.header("所选行政区地图展示")
 # st.image("img/Lenna.png", "行政区划图：{}, {}{}".format(year, province, city))
 # st.image("data/countypoint_1996_湖北省_sample.shp", "行政区划图：{}, {}{}".format(year, province, city))
-st.image("img/{}/province/{}.png".format(year, province), "行政区划图：{}年, {}{}".format(year, province, city))
+try:
+    st.image("img/{}/province/{}.png".format(year, province), caption="行政区划图：{}年, {}".format(year, province))
+except FileNotFoundError as e:
+    st.write("File " + e.filename + " do not exist")
+
+changes = ReadExcel.getdata(year, province)
 
 st.header("行政区划具体变化")
-st.markdown("**{}：**2019年末，全省常住人口3973万人，其中城镇常住人口2642万人。\
-            辖9个地级市，29个市辖区、12个县级市、44个县（合计85个县级行政区划单位），\
-            184个街道、653个镇、251个乡、19个民族乡（合计1107个乡级行政区划单位），\
-            2649个居委会、14355个村委会。".format(province))
-st.markdown("> 读xlsx还没有做所以现在选不同行政区这里还不会变orzzz")
+st.markdown("**{}：**".format(province) + changes['description'])
+if len(changes) > 1:
+    st.subheader("地级市")
+    st.write(changes)
+    df = pd.DataFrame(
+        [(k, v) for k, v in changes.items() if k != 'description'],
+        columns = ['类别', '内容']
+    )
+    tb = st.table(df)
