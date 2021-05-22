85a6877cf89784578fad824aaf3ffeb394de58e4
Asudy <18906052155@189.cn>
2021-05-22 14:59:41 +0800

    style(GetProvince): re-format

--- GetProvinceNameByYear.py
+++ GetProvinceNameByYear.py
@@ -1,15 +1,19 @@
-def GetProvinceNameByYear(year):
-    list_Province = ['青海','重庆','海南','广东','北京市', '天津市', '上海市', '河北' , '浙江', '福建', '江苏', '山东', '山西', '安徽','江西','河南',	'湖北',	'湖南',	'内蒙古',	'广西',	'四川',	'贵州',	'云南',	'西藏',	'陕西',	'甘肃',	'宁夏',	'新疆',	'辽宁',	'吉林', '黑龙江']
-    if((year>=1980 and year<=1987) or year==1991):
-        #27
+from typing import Literal
+
+
+def GetProvinceNamesByYear(year):
+    list_Province = ['青海', '重庆', '海南', '广东', '北京市', '天津市', '上海市', '河北', '浙江', '福建', '江苏', '山东', '山西', '安徽',
+                     '江西', '河南', '湖北', '湖南', '内蒙古', '广西', '四川', '贵州', '云南', '西藏', '陕西', '甘肃', '宁夏', '新疆', '辽宁', '吉林', '黑龙江']
+    # if year >= 1980 and year <= 1987 or year == 1991:
+    if year <= 1991:
+        # 27
         return list_Province[3:]
-    elif(year>=1992 and year<=1996):
-        #28
+    elif year <= 1996:
+        # 28
         return list_Province[2:]
-    elif(year==2015):
+    else:
         return list_Province
 
-#main
 
-list = GetProvinceNameByYear(1982)
+list = GetProvinceNamesByYear(1991)
 print(list)
