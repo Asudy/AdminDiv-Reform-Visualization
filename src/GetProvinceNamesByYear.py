a16ff35947af3c51fcdc075d05b37a52d713f359
Asudy <18906052155@189.cn>
2021-06-08 17:16:41 +0800

    test(GetProvince): add unit test code

--- src/GetProvinceNamesByYear.py
+++ src/GetProvinceNamesByYear.py
@@ -1,4 +1,4 @@
-def GetProvinceNamesByYear(year):
+def GetProvinceNamesByYear(year: int):
     list_Province = ['海南', '青海', '广东', '河北', '浙江', '福建', '江苏', '山东', '山西', '安徽', '江西', '河南',
                      '湖北', '湖南', '内蒙古', '广西', '四川', '贵州', '云南', '西藏', '陕西', '甘肃', '宁夏', '新疆', '辽宁', '吉林', '黑龙江']
 
@@ -8,5 +8,11 @@ def GetProvinceNamesByYear(year):
     return list_Province[1:]
 
 
-#list = GetProvinceNamesByYear(1991)
-# print(list)
+if __name__ == '__main__':
+    try: 
+        while True:
+            y = int(input('Input year: '))
+            list = GetProvinceNamesByYear(y)
+            print(list)
+    except KeyboardInterrupt:
+        print('\nKeyboard interrupt received. Exiting.')
