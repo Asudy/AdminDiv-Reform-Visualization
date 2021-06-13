7c14e28834b9e4f1f077cbb8b79d702abea53ea8
Asudy <18906052155@189.cn>
2021-06-14 04:16:35 +0800

    refactor(main): minor modifications

--- src/main.py
+++ src/main.py
@@ -8,32 +8,18 @@ if __name__ == "__main__":
         page_title = "中国行政区划改革",
         page_icon = '🇨🇳',
         initial_sidebar_state="expanded",
-        layout='wide'
+        # layout='wide'
     )
 
     ### Set title
-    st.title("🇨🇳中国行政区划改革（1978~2020）")
+    st.title("🇨🇳中国行政区划改革（1977~2020）")
 
     ### Set sidebar
     st.sidebar.header("功能")
-    func = st.sidebar.radio('', [0, 1], index=1, 
+    func = st.sidebar.radio('', [0, 1], 
         format_func=lambda x: ['具体行政区划变化查询', '行政区划变化统计信息查询'][x])
 
     if (func == 0):
         SelectAndDisplaySingleChange()
     else:
         DisplayStatistics()
-        # a, b = st.checkbox('a'), st.checkbox('b')
-        # c = st.checkbox('c')
-        # show_dic = {}
-        # if a:
-        #     show_dic['a'] = {}
-        #     for k, v in zip(range(1978, 1984), [1,2,1,3,1,4]):
-        #         show_dic['a'].update({k: v})
-        #     st.write(show_dic)
-        # if b:
-        #     show_dic['b'] = [2,4,2,7,5,8]
-        # if c:
-        #     # show_dic['c'] = {1978: 1, 1979: 3, 1980: -2, 1981: 4, 1982: 2, 1983: 1}
-        #     show_dic['c'] = {'1978': 1, '1979': 3, '1980': -2, '1981': 4, '1982': 2, '1983': 1}
-        # st.area_chart(show_dic, use_container_width=True)
