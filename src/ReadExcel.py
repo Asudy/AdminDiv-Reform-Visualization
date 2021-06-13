96f0dc9baf729b25616c86a1ace674e7eaadec94
Asudy <18906052155@189.cn>
2021-06-13 14:27:32 +0800

    refactor(ReadExcel): add type annotations & rename func

--- src/ReadExcel.py
+++ src/ReadExcel.py
@@ -9,7 +9,7 @@ import pandas as pd
 province = {'北京': 0, '天津': 1, '河北': 2, '浙江': 3, '福建': 4, '上海': 5, '江苏': 6, '山东': 7, '广东': 8, '海南': 9, '山西': 10, '安徽': 11, '江西': 12, '河南': 13, '湖北': 14, '湖南': 15,
             '内蒙古': 16, '广西': 17, '重庆': 18, '四川': 19, '贵州': 20, '云南': 21, '西藏': 22, '陕西': 23, '甘肃': 24, '青海': 25, '宁夏': 26, '新疆': 27, '辽宁': 28, '吉林': 29, '黑龙江': 30}
 
-def getFrequency():
+def GetStatistics() -> dict:
     d = pd.read_excel(r"data/statistics/地级市.xlsx", sheet_name="按年份分", index_col=0,
                            header=0, keep_default_na=False)
     #print("输出列标题",d.columns.values)
@@ -126,7 +126,7 @@ def getdata(year, provincename, type=""):
     return Dict
 
 
-def ReadExcel(year, provincename):
+def ReadExcel(year: int, provincename: str) -> dict:
     Dict = dict()
     dic1 = getdata(year, provincename, '地级市')
     dic2 = getdata(year, provincename, '县级市')
