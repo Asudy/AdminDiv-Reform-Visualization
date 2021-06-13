a0227350da3a5403a88127bd19981e018bddcdb5
Asudy <18906052155@189.cn>
2021-06-14 00:57:47 +0800

    feat(stat): add dict reformer

--- src/ReformStatistics.py
+++ src/ReformStatistics.py
@@ -1,11 +1,27 @@
 import streamlit as st
 import altair as alt
-from ReadExcel import ReadExcel
-from GetProvinceNamesByYear import GetProvinceNamesByYear
+from ReadExcel import GetStatistics
 
-from t_GetStatistics import t_GetStatistics
 
 levels = ['地级市', '县级市', '市辖区']
+
+
+def _reformDict(d: dict) -> dict:
+    yearList = list(map(str, range(1978, 2021)))
+
+    for v in d.values():
+        v['年份'] = yearList
+        for kk, vv in v.items():
+            v[kk] = vv[:len(yearList)]
+
+    # for v in d.values():
+    #     for kk, vv in v.items():    # kk: str, vv: list[int]
+    #         dd = dict()
+    #         for kkk, vvv in zip(yearList, vv[:len(yearList)]):
+    #             dd.update({kkk: vvv})
+    #         v[kk] = dd
+    return d
+
 
 def DisplayStatistics():
     st.sidebar.header('视图')
@@ -14,23 +30,25 @@ def DisplayStatistics():
     level = st.sidebar.radio('', levels)
     
     st.markdown("<center> <h2> 行政区变化统计信息 </h2> </center>", unsafe_allow_html=True)
-    t_dic = t_GetStatistics()
+    stats = GetStatistics()
+    _reformDict(stats)
     # st.write(t_dic)
+    # st.write(_reformDict(t_dic))
     
-    # for level in levels:
     st.header(level)
     show_dict = dict()
 
-    modes = list(t_dic[level].keys())
+    modes = list(stats[level].keys())
     cols = st.beta_columns(len(modes))
 
     for i in range(len(cols)):
         key = modes[i]
         with cols[i]:
             if st.checkbox(key, key=level + '_' + key):
-                show_dict[key] = t_dic[level][key]
+                show_dict[key] = stats[level][key]
     
     # st.write(show_dict)
     if len(show_dict):
         st.line_chart(show_dict, height=200, use_container_width=True)
         st.area_chart(show_dict, height=200, use_container_width=True)
+        st.bar_chart(show_dict, height=200, use_container_width=True)
